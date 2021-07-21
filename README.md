fbc751fb45afc1899deb4c30f0aaec624dd17214
stephenmcfarland <snmcfarland@gmail.com>
2021-07-21 16:54:27 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-"# proj01" 
+REIMBURSEMENT SYSTEM
+Project Description
+This project allows a user to login perform full CRUD operations on an employee table and reimbursement table by adding employee details and submitting a reimbursement request. A user may view all reimbursement requests or lookup requests by user name. A manager can view all pending reimbursement requests. A manager can update the status of any reimbursement request. 
+
+Technologies 
+
+Java
+Jackson ORM
+Postgresql
+Javascript
+HTML
+CSS
+
+Features
+
+Update employee information
+Change status of reimbursement
+View all reimbursements
+
+To-do List
+
+Distinguish between user and manager roles
+Consolidate steps between views
+
+Getting Started
+
+clone git@github.com:StephenMcFarland/proj01.git
+
+
+Usage
+
+After cloning open project folder in IDE. Add project to server. Run project. Open in browser at
+
+http://localhost:8080/ReimbursementSystem/user.html
+
+Enter your email address, continue to application, when adding an employee or submitting a reimbursement request view existing employees or reimbursements so as to input a unique id yet to be used. The system will not save the record unless it is a unique number. The system will not tell you if your save was successful. To see if the save or update was successful review the view all links available, "Display all employees" and "Manager display all reimbursements."
---
+++ src/main/webapp/js/musixmatch.js
@@ -0,0 +1,100 @@
+//let text = `{"message":{"header":{"status_code":200,"execute_time":0.008936882019043},"body":{"lyrics":{"lyrics_id":6288769,"explicit":0,"lyrics_body":"Sometimes I feel like I don't know\nSometimes I feel like checking out\nI wanna get it wrong\nCan't always be strong\nAnd love it won't be long ...\n\nOh sugar, don't you cry\nOh child, wipe the tears from your eyes\nYou know I need you to be strong\nAnd the day is dark, as the night is long\nFeel like trash\nYou make me feel clean\nI'm in the black\n...\n\n******* This Lyrics is NOT for Commercial use *******","script_tracking_url":"https:\/\/tracking.musixmatch.com\/t1.0\/m_js\/e_1\/sn_0\/l_6288769\/su_0\/rs_0\/tr_3vUCAFS8hiFtBItzz4-K_tCNJ-_J9nquE7B3fM5B0dDCGv5TmUcggy4oWXYwmZjcSxafQlr9gcKTqDMAzqw8j-UtXDILWfoCH5FQvTWxsU2YT8vg3VCG3Hj4QHn79kO96UZ5-ZWRjLeMExNlJqFqKRF_yqj6BmDZz00OlYNqI_Cd36RSHd__a496UNaaCfQuCrL760bnDZRH03UY8KuOOdqLLWpWVyzbDoE0K5Img3PuXtc3qYhycVFW4sXWqc7CeWHe6wD5nMAPwfgymLRTSV9-zgKk8EJMDsZHtWbB6kXHrXScRGEvrWqIfEQ6GfJ4GnlUwbzbrxowrdyQnK0w7vyHFUhvT6m--U4AV3okNDMCNaBlZYvJGEmPUQLi45g8y6hA79sGDLLfylcoSV2yvRHO8kwjuXdNz9rKW5UYCA\/","pixel_tracking_url":"https:\/\/tracking.musixmatch.com\/t1.0\/m_img\/e_1\/sn_0\/l_6288769\/su_0\/rs_0\/tr_3vUCAJ9mgUEKBvPfwmMV-3Z18Ckj7MsAuMN5PmlJDzUqLtIVFJDlT7dESEAvsyqeCOQr4pmNqX2e6kBHcEDa_qljpAMAIswJbsP6qSfqSF1TZC3t6-9DOjI9sRjOCLW0q_GRm9GoV_ABkfHwSy-Tv_xG7aP1fEeRpIPguHnmBzq3vD3k5CgCYz0PRRc2Sxq9Pagig7v4SUOEMtnKjKTVLKWbpRV15xXIRRmEdgTdL24LoAKMFmVjd5fhKnPQY1TzVMOLYRtmHPaX0HZ4mObyQw8gVZQTIiX3YjZDp3QEQ5or6kqJYewRjYy91hTKstKOOW20JFJ18_CpjPb--pL74EFzB5DFImmEyCwBNLRDWScey0TTkijOtPCHxOFryXf9UzRftqC6sCRlZmKhsqW4clrvjeEusa9qZiH8kwUl1g\/","lyrics_copyright":"Lyrics powered by www.musixmatch.com. This Lyrics is NOT for Commercial use and only 30% of the lyrics are returned.","updated_time":"2021-05-24T13:41:02Z"}}}}`;
+let text = `{
+  "message": {
+    "header": {
+      "status_code": 200,
+      "execute_time": 0.0089290142059326
+    },
+    "body": {
+      "lyrics": {
+        "lyrics_id": 6288745,
+        "explicit": 0,
+        "lyrics_body": "You're dangerous, 'cos you're honest.nYou're dangerous, you don't know what you want.nWell you left my heart empty as a vacant lotnFor any spirit to haunt.nnYou're an accident waiting to happennYou're a piece of glass left there on a beach.nWell you tell me thingsnI know you're not supposed tonThen you leave me just out of reach.nnWho's gonna ride your wild horses?nWho's gonna drown in your blue sea?nWho's gonna ride your wild horses?nWho's gonna fall at the foot of thee?n...nn******* This Lyrics is NOT for Commercial use *******",
+        "script_tracking_url": "https://tracking.musixmatch.com/t1.0/m_js/e_1/sn_0/l_6288745/su_0/rs_0/tr_3vUCAMt_ZZzSG8yxOBrlm-JuXh3D1sgTZSjihFqkwn4JnWdPmWkdHJ_5hN06jgB4mmuY9lUrCd0YkoVCYfRBdsIFInsKrd8XPFNDy48kvI087n3pDwZcZDA0SQDPsJ9kE18wpInpN54sgRai7S80Ayq_ejhMbzTJ1711qaNWjE_0CelhGi0qbClhYibV-q0OgjWyeIG0-qeKg4F64HR-GRc0bSfq_WMc6KgKlo2WNklJg9ad2iBioaOaBXl8qUCl2EZagxANIhaBgFg9X5Ny28yFi-rXhCu0F4VTI6LdvQpLVt_QCHirJL997W0SMfJapVvvAhKWls37oZiIx2WiAd7jUEvLMoR73ZvAu8L8CKEa9qZKplbXLKOxGYLqKoZCSmPlpb3BOKDIy_Q8hX-W7nb3Nse-sKB_FdgzepkH0yErrw/",
+        "pixel_tracking_url": "https://tracking.musixmatch.com/t1.0/m_img/e_1/sn_0/l_6288745/su_0/rs_0/tr_3vUCAIxo1Uhojmbia11OwuVyv4ifBf42vg2x9Fx_tepRB9LLEOE2UrG_FS0nZWYSWybaY2bXluJJcB00FS6LCYnZ7LE-1N5x7rAQzubhBW-QdGGDoSVGdIW-LkpFacuUulMw6aGJd2ri6k3p5pSsCHnZFwU0i6bx0qbMS56IdIjf6igk9QfvsfPjL-tenZwhoEcWMtK1C7IxG9FyPxYXurNgOABUrb378v0sVx_XtS9GXLsoCBrVKp9bUmDvbHbLpKq574vXCsa54zddgVrDwYgSnomww_mrFJlzr3VEQnjqQfOMSpI8omBQsC5m7wHyEQZc37wtOIVKHDYMsQ1EVGoOX1fKGc_MlZzeBhzsLflGL_0-PQhvZCxULIUiQ_19AZFUPiI6-MXeKg9nfjqOM0mHgHbbpW_GbB5cAar51pC3Hw/",
+        "lyrics_copyright": "Lyrics powered by www.musixmatch.com. This Lyrics is NOT for Commercial use and only 30% of the lyrics are returned.",
+        "updated_time": "2021-04-11T13:35:30Z"
+      }
+    }
+  }
+}`;
+document.addEventListener('DOMContentLoaded', async()=>{
+	
+	const reimsResp = await fetch('https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=17507003&apikey=8c834407ae6a66e83108e7a075e384a1',{mode:'no-cors'});
+	//const reimsResp = await fetch('https://swapi.dev/api/planets/1/');//,{mode: 'no-cors'});
+	//const reims = await reimsResp.json();
+	const reims = await reimsResp.json();
+	//let newstr = reims.replace('\\', '');
+	console.log(reims);
+	//console.log(reims);
+	//swapi 
+/*	document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='owner'>${reims.name}</span><span class='owner'>${reims.rotation_period}</span><span class='owner'>${reims.orbital_period}</span><span class='owner'>${reims.diameter}</span><span class='owner'>${reims.climate}</span><span class='reimAmount'>${reims.gravity}</span><span class='owner'>${reims.population}</span></div>`;
+	var Container = document.getElementById("reims");
+	reims.residents.forEach(reim =>{
+		var div = document.createElement("div");
+		div.classList.add('reimbursement');
+		div.innerHTML = `<span class='reimId'>${reim}</span>`;
+		Container.appendChild(div);
+})*/
+	})
+
+
+//const obj = JSON.parse(text);
+//const data = require('./js/jm.json');
+//console.log(obj);
+
+//document.getElementById("keys").innerHTML = `<div class='reimbursement'><span>${obj.message.body.lyrics.lyrics_body}</span></div>`;
+//console.log(obj.items[0].external_urls.spotify);
+//console.log(obj.items.external_urls);
+//document.addEventListener('DOMContentLoaded', async()=>{
+	
+/*	fetch("http://localhost:8080/ReimbursementSystem/webapp/js/jm.json")
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data));*/
+/*	var Container = document.getElementById("reims");
+		//obj.forEach(items =>{
+			for(var i=0;i<obj.items.length;i++){
+		var div = document.createElement("div");
+		div.classList.add('reimbursement');
+		div.innerHTML = `<span class='reimId'>Disc#: ${obj.items[i].disc_number}</span><span class='owner'>${obj.items[i].duration_ms} ms</span><span class='owner'>Track Title: ${obj.items[i].name}</span><span class='owner'> <a href='${obj.items[i].external_urls.spotify}' target='blank'>Play song on spotify</a> </span>`;
+		Container.appendChild(div);
+		}*/
+//})
+	
+	
+/*async function myfunction() {
+  console.log('Inside of myfunction');
+}
+
+(async() => {
+  console.log('before start');
+
+  await start();
+  const reimsResp = await fetch('https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=17507003&apikey=8c834407ae6a66e83108e7a075e384a1');
+  console.log('after start');
+})();*/
+
+
+//fetch('https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=17507003&apikey=8c834407ae6a66e83108e7a075e384a1',{mode: 'no-cors'})
+//.then(response => response.json())
+//.then(data => console.log(data));
+	
+/*	document.addEventListener(DOMContentLoaded,async() => {
+	const reimsResp = await fetch('https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=17507003&apikey=8c834407ae6a66e83108e7a075e384a1');
+	const reims = await reimsResp.json();
+	console.log(reims);
+	})*/
+/*	document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span><span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span><span class='status'> Status</span></div>`;
+	var Container = document.getElementById("reims");
+	reims.forEach(items =>{
+		var div = document.createElement("div");
+		div.classList.add('reimbursement');
+		div.innerHTML = `<span class='reimId'>${items.name}</span><span class='owner'> ${items.external_urls.spotify} </span>`;
+		Container.appendChild(div);
+})*/
+	//})
+	
+	
+	
