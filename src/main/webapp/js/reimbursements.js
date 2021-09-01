3d88b56dd20868371541b5dfa4cd78e4255cd33f
Stephen McFarland <snmcfarland@gmail.com>
2021-09-01 17:15:29 -0400

    count looks better?

--- src/main/webapp/js/reimbursements.js
+++ src/main/webapp/js/reimbursements.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async()=>{
 	const reimsResp = await fetch('http://localhost:8080/ReimbursementSystem/getReims');
 	const reims = await reimsResp.json();
 	console.log(reims);
-	document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span><span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span><span class='status'> Status</span><span>Reimbursements: ${reims.length}</span></div>`;
+	document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span><span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span><span class='status'> Status</span><span class='owner'>Count: ${reims.length}</span></div>`;
 	var Container = document.getElementById("reims");
 	reims.forEach(reim =>{
 		var div = document.createElement("div");
