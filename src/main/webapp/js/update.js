8265b1377d148a48be484604648cab1434b4a238
Stephen McFarland <snmcfarland@gmail.com>
2021-08-31 20:45:48 -0400

    Employee update form ready to be pieced in update.js

--- src/main/webapp/js/update.js
+++ src/main/webapp/js/update.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async()=>{
 	const reimsResp = await fetch('http://localhost:8080/ReimbursementSystem/getEmpUp');
 	const reims = await reimsResp.json();
 	console.log(reims);
-	document.getElementById("fieldnames").innerHTML = `<div>test</div>`;//<div class='reimbursement'><span class='reimId'>id</span><span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span><span class='status'> Status</span></div>`;
+	document.getElementById("fieldnames").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span><span class='owner'> Name</span><span class='reimAmount'> Phone</span><span class='status'> Address</span><span class='status'> Email</span></div>`;
 	var Container = document.getElementById("empdetails");
 	reims.forEach(emp =>{
 		var div = document.createElement("div");
