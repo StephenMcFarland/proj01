a3d6d7b9cadc9e8f0d5c4981ccbb73aa2d7c3cde
Stephen McFarland <snmcfarland@gmail.com>
2021-09-02 16:51:00 -0400

    CSS auto color code Status in Reimbursement view

--- src/main/webapp/js/reimbursements.js
+++ src/main/webapp/js/reimbursements.js
@@ -2,20 +2,20 @@ document.addEventListener('DOMContentLoaded', async()=>{
 	const reimsResp = await fetch('http://localhost:8080/ReimbursementSystem/getReims');
 	const reims = await reimsResp.json();
 	console.log(reims);
-<<<<<<< Updated upstream
-	document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span><span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span><span class='status'> Status</span><span class='owner'>Count: ${reims.length}</span></div>`;
-=======
-	document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span>
-	<span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span>
-	<span class='status'> Status</span></div>`;
->>>>>>> Stashed changes
+
+	//document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span><span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span><span class='status'> Status</span><span class='owner'>Count: ${reims.length}</span></div>`;
+
+	//document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span>
+	//<span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span>
+	//<span class='status'> Status</span></div>`;
+
 	var Container = document.getElementById("reims");
 	reims.forEach(reim =>{
 		var tr = document.createElement("tr");
 		tr.classList.add('reimbursement');
 		tr.innerHTML = `<td class='reimId'>${reim.reimId}</td><td class='owner'> ${reim.reimOwner} </td>
 		<td class='resolver'>  ${reim.reimResolver} </td><td class='reimAmount'>$${reim.reimAmount}</td> 
-		<td class='status'> ${reim.reimStatus}</td>	
+		<td data-status='${reim.reimStatus}' class='status'></td>	
 		<td><form method='get' action='updateReim'>
 <select name='status' id='status'>
   <option value='pending'>pending</option>
