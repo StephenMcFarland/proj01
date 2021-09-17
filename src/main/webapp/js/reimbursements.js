23c91a0dc5ce60d055c05fe454c8b15864fa7bfe
Stephen McFarland <snmcfarland@gmail.com>
2021-09-17 17:02:51 -0400

    Add Currency Format
    https://stackabuse.com/how-to-format-number-as-currency-string-in-javascript/

--- src/main/webapp/js/reimbursements.js
+++ src/main/webapp/js/reimbursements.js
@@ -1,4 +1,8 @@
+
+
 document.addEventListener('DOMContentLoaded', async()=>{
+	
+
 	const reimsResp = await fetch('http://localhost:8080/ReimbursementSystem/getReims');
 	const reims = await reimsResp.json();
 	console.log(reims);
@@ -8,13 +12,17 @@ document.addEventListener('DOMContentLoaded', async()=>{
 	//document.getElementById("keys").innerHTML = `<div class='reimbursement'><span class='reimId'>id</span>
 	//<span class='owner'> Owner</span><span class='resolver'> Resolver</span><span class='reimAmount'> Amount</span>
 	//<span class='status'> Status</span></div>`;
-
+	
 	var Container = document.getElementById("reims");
 	reims.forEach(reim =>{
 		var tr = document.createElement("tr");
+		let dollarUS = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+});
 		tr.classList.add('reimbursement');
 		tr.innerHTML = `<td class='reimId'>${reim.reimId}</td><td class='owner'> ${reim.reimOwner} </td>
-		<td class='resolver'>  ${reim.reimResolver} </td><td class='reimAmount'>$${reim.reimAmount}</td> 
+		<td class='resolver'>  ${reim.reimResolver} </td><td class='reimAmount'>${dollarUS.format(reim.reimAmount)}</td> 
 		<td data-status='${reim.reimStatus}' class='status'></td>	
 		<td><form method='get' action='updateReim'>
 <select name='status' id='status'>
